6a69db4caf2b9ca30f475d449f5d4c864c8f302f
Kunal Gupta <kunal.gupta@myself.com>
2017-12-10 00:41:12 +0530

    QR Work.

--- pi/hascodes.py
+++ pi/hascodes.py
@@ -35,9 +35,7 @@ class GetCodes:
         zbar_image = zbar.Image(width, height, 'Y800', image.tobytes())
         scanner = zbar.ImageScanner()
         scanner.scan(zbar_image)
-        cnt=0
         for decoded in zbar_image:
-            cnt+=1
             if decoded.type is zbar.Symbol.EAN13:
                 self.hasBar=True
                 self.bar.append(decoded)
@@ -46,4 +44,3 @@ class GetCodes:
                 self.qr.append(decoded)
             else:
                 self.others=True
-        print cnt
