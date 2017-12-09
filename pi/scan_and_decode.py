32eb2b76c6adb9839aa4399581f86e6ede3e4824
Kunal Gupta <kunal.gupta@myself.com>
2017-12-10 00:41:12 +0530

    QR Work.

--- pi/scan_and_decode.py
+++ pi/scan_and_decode.py
@@ -27,20 +27,20 @@ def main():
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(gray,127,255,1)
-        cv2.imshow('Gray', np.hstack([gray,thresh]))
+#        cv2.imshow('Gray', np.hstack([gray,thresh]))
         codes = hascodes.GetCodes()
         codes.scan(gray)
-#        if codes.hasQR:
-##            print len(codes.qr)
-#            for c in codes.qr:
-#                print c.data
+        if codes.hasQR:
+#            print len(codes.qr)
+            for c in codes.qr:
+                print c.data
         if codes.hasBar:
 #            print len(codes.bar)
             for c in codes.bar:
                 print c.data
         print "Frame Done"
         triangle = detecttriangle.DetectTriangle(100, gray)
-        triangle_present = triangle.has_triangle(frame, True)
+        triangle_present = triangle.has_triangle(frame, DEBUG=False)
         triangle_location = triangle.location
     return True
 
