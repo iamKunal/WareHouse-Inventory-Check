513c10ee60a2c554c0c0ad96852b2007f3be405b
Kunal Gupta <kunal.gupta@myself.com>
2017-12-13 01:01:02 +0530

    Added Line Detection Code and Line follower code.

--- follow_line.py
+++ follow_line.py
@@ -8,7 +8,7 @@ import numpy as np
 import detectrectangles
 
 # IP webcam URL
-url = 'http://192.168.0.105:4747/cam/1/frame.jpg'
+url = 'http://192.168.1.7:4747/cam/1/frame.jpg'
 def main():
     # # Select and start Camera.
     # DEVICE_NO = 0
@@ -24,17 +24,46 @@ def main():
             cv2.destroyAllWindows()
             break
         height,width = img.shape[:2]
-        frame = img[10:width-10,0:height]
+        frame = img[15:width-15,0:height]
         # ret, frame = capture.read()
         # if ret==False:
         #     print "Error !"
         #     return False
-        cv2.imshow('Video Capture', frame)
+        ## Gaussian Blur for better 
+        frame = cv2.GaussianBlur(frame,(15,15),0)
         line = get_yellow.get_frame(frame, DEBUG=False)
 #        cv2.imshow("yellow", get_yellow.get_frame(frame))
-        cv2.imshow('Canny', cv2.Canny(line,100,200))
-        # rectangle = detectrectangles.Rectangle(100, line)
-        # rectangle.has_rectangle(img=None, DEBUG=False)
+        # cv2.imshow('Canny', cv2.Canny(line,100,200))
+        canny = cv2.Canny(line,100,200)
+        rectangle = detectrectangles.Rectangle(100, line)
+        rectangle.has_rectangle(img=frame, DEBUG=True)
+        if rectangle.contour is not None:
+            ## Centroid of the area
+            M = cv2.moments(rectangle.contour)
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            cv2.circle(frame, (cx,cy), 10, (0,0,225), 5)
+            
+            ## Get all points inside the yellow line
+            cnt=np.array([a[0] for a in rectangle.contour])
+            ## Get line of best fit
+            [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+            ## Get angle between this line and vertical axes.
+            angle = 180*detectrectangles.angle([0,1], [vx,vy])/np.pi
+            turn='right'
+            ## Representation of vx, vy when drone is facing respective direction:
+            ## (++) F (+-)
+            ## --L--+--R--
+            ## (+-) B (++)
+            
+            if(vy<0):
+                turn='left'
+            print 'turn',turn,angle,'degrees.'
+            ## Plot our line on the image.
+            lefty = int((-x*vy/vx) + y)
+            righty = int(((frame.shape[1]-x)*vy/vx)+y)
+            cv2.line(frame,(frame.shape[1]-1,righty),(0,lefty),255,2)
+        cv2.imshow('Video Capture', frame)
     return True
 
 if __name__ == "__main__":
